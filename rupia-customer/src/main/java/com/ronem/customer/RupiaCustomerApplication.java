f37c21a7630695edabbc80176dd6af76a7a2b0e8
Ram Mandal <ram.mandal@8squarei.com>
2026-01-15 11:53:04 +0545

    added submodules for rupia

---
+++ rupia-customer/src/main/java/com/ronem/customer/RupiaCustomerApplication.java
@@ -0,0 +1,13 @@
+package com.ronem.customer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupiaCustomerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RupiaCustomerApplication.class, args);
+    }
+
+}
---
+++ rupia-transaction-service/src/test/java/com/ronem/transactionservice/RupiaTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronem.transactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupiaTransactionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
