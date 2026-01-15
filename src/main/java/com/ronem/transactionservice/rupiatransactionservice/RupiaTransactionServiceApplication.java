370c38f1e3e33b35d750b8180ec1ac7bfe64b300
Ram Mandal <ram.mandal@8squarei.com>
2026-01-15 12:16:33 +0545

    initial commit

---
+++ src/main/java/com/ronem/transactionservice/rupiatransactionservice/RupiaTransactionServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ronem.transactionservice.rupiatransactionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RupiaTransactionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RupiaTransactionServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ronem/transactionservice/rupiatransactionservice/RupiaTransactionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ronem.transactionservice.rupiatransactionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RupiaTransactionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
